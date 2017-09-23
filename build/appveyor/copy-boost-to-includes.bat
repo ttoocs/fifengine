f7321b7d3582ca1ffdd8747b408080db0f379cb6
Thomas Kowaliczek <linuxdonald@posteo.de>
2017-09-23 17:21:06 +0200

    Update copy-boost-to-includes.bat

--- build/appveyor/copy-boost-to-includes.bat
+++ build/appveyor/copy-boost-to-includes.bat
@@ -10,10 +10,10 @@ mkdir C:\projects\fifengine-dependencies\includes\include\boost
 xcopy /Y /S "C:\Libraries\boost_1_64_0\boost" "C:\projects\fifengine-dependencies\includes\include\boost" >nul 2>&1
 
 :: copy binaries (dlls) and libs
-copy "C:\Libraries\boost_1_64_0\lib32-msvc-15.0\boost_filesystem-vc150-mt-1_64.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_filesystem-vc150-mt-1_64.dll"
-copy "C:\Libraries\boost_1_64_0\lib32-msvc-15.0\boost_system-vc150-mt-1_64.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_system-vc150-mt-1_64.dll"
-copy "C:\Libraries\boost_1_64_0\lib32-msvc-15.0\libboost_filesystem-vc150-mt-1_64.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_filesystem-vc150-mt-1_64.lib"
-copy "C:\Libraries\boost_1_64_0\lib32-msvc-15.0\libboost_system-vc150-mt-1_64.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_system-vc150-mt-1_64.lib"
+copy "C:\Libraries\boost_1_64_0\lib32-msvc-14.1\boost_filesystem-vc141-mt-1_64.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_filesystem-vc141-mt-1_64.dll"
+copy "C:\Libraries\boost_1_64_0\lib32-msvc-14.1\boost_system-vc141-mt-1_64.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_system-vc141-mt-1_64.dll"
+copy "C:\Libraries\boost_1_64_0\lib32-msvc-14.1\libboost_filesystem-vc141-mt-1_64.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_filesystem-vc1410-mt-1_64.lib"
+copy "C:\Libraries\boost_1_64_0\lib32-msvc-14.1\libboost_system-vc141-mt-1_64.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_system-vc141-mt-1_64.lib"
 
 :: remove the "share" folder
 rmdir /S /Q C:\projects\fifengine-dependencies\includes\share
